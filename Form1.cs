319684ecf897dca4bd8adc91995a938cf6c11953
Administrator <Administrator@LAB1-MAY09>
2024-06-24 16:39:05 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyThongTin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Soure=LAB1-MAY15\MISASME2022;InitialCatalog=QuanLyThongTin;Integrated Security=True;Encrypt=False");
+
+        private void openCon()
+        {
+            if (con.State == ConnectionState.Closed) {
+                con.Open();
+
+            }
+        }
+        
+        private void closeCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        private Boolean Exe(string cmd)
+        {
+            openCon();
+            Boolean check;
+            try
+            {
+
+            }
+            catch {
+                
+            closeCon();
+            return check;
+        }
+        private void
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
